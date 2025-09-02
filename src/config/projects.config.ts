974c479db3abbc908f9510b581d4954009f82007
schwar_Zzz <schwarz@pc.localdomain>
2025-09-02 15:05:47 +0100

    weather project

--- src/config/projects.config.ts
+++ src/config/projects.config.ts
@@ -1,4 +1,5 @@
-import pic from '@/assets/words-with-backgrounds.gif'
+import pic from '@/assets/words-with-backgrounds.gif' 
+import pic2 from '@/assets/Screenshot145954.png'
 
 export const projects = [
   {
@@ -11,6 +12,16 @@ export const projects = [
     githubUrl: 'https://github.com/schwar-Zzz/wordl-clone',
     featured: true
   },
+  {
+    id: '2',
+    title: 'Weather app',
+    description: 'Weather App – Built with React to fetch and display real-time weather data, with error handling and persistent search history. Optimized performance using React.memo, useCallback, and lifted state for smooth component interaction.',
+    image: pic2,
+    stack: ['React', 'Css', 'Hooks', 'openweathermap api'],
+    liveUrl: 'https://schwrazstudioweather-app.vercel.app/',
+    githubUrl: 'https://github.com/schwar-Zzz/Weather_App',
+    featured: true 
+  }
 ];
 
 
