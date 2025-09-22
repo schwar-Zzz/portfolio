8152fb5d1c509b4f90ae0e338a05b94b922ddb43
schwar_Zzz <schwarz@pc.localdomain>
2025-09-22 13:07:29 +0100

    conflict

--- src/config/portfolio.config.ts
+++ src/config/portfolio.config.ts
@@ -7,7 +7,7 @@ export const portfolioConfig= {
   links: {
     github: "https://github.com/schwar-Zzz",
     twitter: "https://x.com/hdfn_zzz",
-    mail: "ToMail:abdelbasset.hadfani@gmail.com",
+    mail: "mailto:abdelbasset.hadfani@gmail.com",
     discord: "https://discord.com/users/455218843892056084",
     linkedin: "https://www.linkedin.com/in/abdelbasset-hadfani-441a5319a/",
   }
